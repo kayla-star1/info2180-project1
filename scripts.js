6c48a2cf50bada9bd30111f2928f72efacf8aa3f
Kayla Swift <kayla.swift1302@gmail.com>
2022-10-28 08:51:38 -0500

    Updated CSS and JS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,34 @@
 /* Add your JavaScript to this file */
 windows.onload = function(){
-     let learn = document.getElementsByClassName("btn")
+    var myForm = document.querySelector(#form);
 
-     learn[0].addEventListener("click",)
+    myForm.addEventListener('submit', function (ele) {
+        console.log('form submitted');
+        var valFail =false;
 
+        var email = document.querySelector('#email');
 
+        if(!isValidEmail(email.value.trim())){
+            valFail= true;
+            displayMessage(email, valFail);
+        }
+    });
 
+        function isValidEmail(emailAddress) {
+            return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/.test(emailAddress);
+          }
+        function displayMessage(email, valFail){
+            let msg= document.getElementById("message");
+            if (valFail == true){
+                msg.textContent = "Please enter a valid address."
+            }
+            else{
+                msg.textContent= "Thank you! Your email address"+ email
+            }
+           
+        }
+
+   
 }
+
+
