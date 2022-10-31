635bfea4cbc8e56ed86d0882143ad2e23b3c4c24
Kayla Swift <kayla.swift1302@gmail.com>
2022-10-30 23:20:58 -0500

    Final additions

--- scripts.js
+++ scripts.js
@@ -1,34 +1,25 @@
 /* Add your JavaScript to this file */
 window.onload = function(){
     var myForm = document.querySelector("form");
+    
 
     myForm.addEventListener('submit', function (ele) {
-        console.log('form submitted');
-        ele.preventDefault();
-        var valFail =false;
+    console.log('form submitted');
+    ele.preventDefault();
 
-        var email = document.querySelector('#email');
+    
+    var msg = document.getElementsByClassName("message")[0]
 
-        if(!isValidEmail(email.value.trim())){
-            valFail= true;
-            displayMessage(email, valFail);
-        }
+    if((document.getElementById("email").value == "")){
+        msg.textContent= "Please enter a valid address"
+        return false;
+    }
+    else{
+        msg.textContent= "Thank you! Your email address" + " "+ email.value
+        return true;
+    }
+        
     });
-
-        function isValidEmail(emailAddress) {
-            return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/.test(emailAddress);
-          }
-        function displayMessage(email, valFail){
-            let msg= document.getElementById("message");
-            if (valFail == true){
-                msg.textContent = "Please enter a valid address."
-            }
-            else{
-                msg.textContent= "Thank you! Your email address"+ email
-            }
-           
-        }
-
    
 }
 
